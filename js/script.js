729ad416aef3b87b733b692b504fde6d6f1fed05
grace22411 <olayinkagrace112.ob@gmail.com>
2020-04-29 13:51:30 -0700

    everything updated

--- js/script.js
+++ js/script.js
@@ -8,19 +8,43 @@ $(document).ready(function () {
   });
 
 
+  toastr.options = {
+    'closeButton': true,
+    'debug': false,
+    'newestOnTop': false,
+    'progressBar': false,
+    'positionClass': 'toast-top-center',
+    'preventDuplicates': false,
+    'showDuration': '0',
+    'hideDuration': '0',
+    'timeOut': '5000',
+    'extendedTimeOut': '1000',
+    'showEasing': 'swing',
+    'hideEasing': 'linear',
+    'showMethod': 'fadeIn',
+    'hideMethod': 'fadeOut',
+  }
+
   $("#submit").click(function(e){
+    $("#submit").append('<i class="fas fa-spinner fa-spin"></i>')
+    $('.fa-spinner').fadeIn();
     e.preventDefault();
     var $fullname = $('#name').val();
     var $email = $('#email').val();
+    var data = {
+      fullname: $fullname,
+      email: $email
+    };
     
       if ($fullname && $email) {
           $.ajax({
           type:'POST',
-          url : 'http://api.ebook.talcube.com/register.php',
-          data : "fullname="+$fullname+"&email="+$email,
+          url: 'http://api.ebook.talcube.com/register.php',
+          data: JSON.stringify(data),
           success: function(success){
             console.log(success);
-            alert('Success registering');
+            toastr.success('Thank you for registering, the ebook has been sent to your mail, kindly check your spam folder if you can’t see it in your inbox.');
+            $('.fa-spinner').fadeOut();
           },
           error: function(xhr, status, error) {
             var err = JSON.parse(xhr.responseText);
@@ -28,7 +52,8 @@ $(document).ready(function () {
           }
       });
       } else {
-        alert('All fields are required')
+        toastr.error('All fields are required');
+        $('.fa-spinner').hide();
       }
       
   });
