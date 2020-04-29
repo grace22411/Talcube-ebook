fc0a1d024994b8e0dc5e424186b5dc10f90b3067
grace22411 <olayinkagrace112.ob@gmail.com>
2020-04-29 10:19:24 -0700

    form authentication

--- js/script.js
+++ js/script.js
@@ -8,6 +8,65 @@ $(document).ready(function () {
   });
 
 
+  $("#submit").click(function(e){
+    e.preventDefault();
+    var $fullname = $('#name').val();
+    var $email = $('#email').val();
+    
+      if ($fullname && $email) {
+          $.ajax({
+          type:'POST',
+          url : 'http://api.ebook.talcube.com/register.php',
+          data : "fullname="+$fullname+"&email="+$email,
+          success: function(success){
+            console.log(success);
+            alert('Success registering');
+          },
+          error: function(xhr, status, error) {
+            var err = JSON.parse(xhr.responseText);
+            console.log(err);
+          }
+      });
+      } else {
+        alert('All fields are required')
+      }
+      
+  });
+    
+
+  // $("#submit").click(function(){
+  //   alert("hello");s
+  // })
+
+
 });
+
+
+// const fullName = document.getElementById('#name');
+// const email = document.getElementById('#email');
+
+// function mySubmitFunction(e) {
+//   e.preventDefault();
+//   var infomation = {
+//     fullName: fullName.val(),
+//     email: email.val()
+//   }
+//   const info = {
+//     method: 'POST',
+//     data: infomation,
+//   }
+//   fetch('http://api.ebook.talcube.com/register.php', info)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((jsonObject) => {
+//       console.log(jsonObject)
+//       document.write(`ID ${jsonObject.id} was created!`);
+//     })
+//     .catch((error) => {
+//       document.write(error);
+
+//     }
+// }
 
 
